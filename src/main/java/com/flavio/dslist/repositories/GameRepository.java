3e77d863865fc2b6dd4dc1b175c1e7e0180592d7
Flavio Assis <flavioh.dev@gmail.com>
2023-05-09 12:25:21 -0300

    Cria GameRepository

---
+++ src/main/java/com/flavio/dslist/repositories/GameRepository.java
@@ -0,0 +1,7 @@
+package com.flavio.dslist.repositories;
+
+import com.flavio.dslist.entities.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+}
