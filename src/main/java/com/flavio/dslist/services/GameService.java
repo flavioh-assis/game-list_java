35f167e043d3ad5af390903512e4e007f0f5d55c
Flavio Assis <flavioh.dev@gmail.com>
2023-05-09 12:38:41 -0300

    Cria service de game

---
+++ src/main/java/com/flavio/dslist/services/GameService.java
@@ -0,0 +1,20 @@
+package com.flavio.dslist.services;
+
+import com.flavio.dslist.dto.GameMinDTO;
+import com.flavio.dslist.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class GameService {
+    @Autowired
+    private GameRepository gameRepository;
+
+    public List<GameMinDTO> findAll() {
+        var result = gameRepository.findAll();
+
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+}
