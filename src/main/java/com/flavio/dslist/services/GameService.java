41684ade05d0ee1278a2b3c8cdb09b884bfd627d
Flavio Assis <flavioh.dev@gmail.com>
2023-05-10 18:21:03 -0300

    Cria service e endpoint de busca de game por id

--- src/main/java/com/flavio/dslist/services/GameService.java
+++ src/main/java/com/flavio/dslist/services/GameService.java
@@ -1,9 +1,11 @@
 package com.flavio.dslist.services;
 
+import com.flavio.dslist.dto.GameDTO;
 import com.flavio.dslist.dto.GameMinDTO;
 import com.flavio.dslist.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,9 +14,17 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         var result = gameRepository.findAll();
 
         return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id) {
+        var foundEntity = gameRepository.findById(id).get();
+
+        return new GameDTO(foundEntity);
     }
 }
